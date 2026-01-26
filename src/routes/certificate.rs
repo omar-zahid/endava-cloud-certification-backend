145cf7bf21f41d4ceef306985daa83ed3e3af0a2
Omar Zahid <omarzahid@outlook.com>
2026-01-26 19:40:05 +0800

    Added filtering

--- src/routes/certificate.rs
+++ src/routes/certificate.rs
@@ -1,13 +1,30 @@
-use axum::{Json, extract::State};
+use axum::{
+    Json,
+    extract::{Query, State},
+};
+use serde::Deserialize;
 
-use crate::{error::AppError, models::certificate::Certificate, state::AppState};
+use crate::{
+    error::AppError,
+    models::certificate::{Certificate, CertificateVendor},
+    state::AppState,
+};
+
+#[derive(Debug, Deserialize)]
+pub struct CertificateFilter {
+    pub vendor: Option<CertificateVendor>,
+}
 
 pub async fn get_certificate(
     State(state): State<AppState>,
+    Query(filter): Query<CertificateFilter>,
 ) -> Result<Json<Vec<Certificate>>, AppError> {
-    let certificates = sqlx::query_as::<_, Certificate>("SELECT * FROM certificates")
-        .fetch_all(&state.pool)
-        .await?;
+    let certificates = sqlx::query_as::<_, Certificate>(
+        "SELECT * FROM certificates WHERE ($1::certificate_vendor IS NULL OR vendor = $1)",
+    )
+    .bind(filter.vendor)
+    .fetch_all(&state.pool)
+    .await?;
 
     Ok(Json(certificates))
 }
